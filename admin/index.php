e62f493bb5008b36874fe84525231310a19e1bf2
kewen <kauakewen1010@hotmail.com>
2024-06-05 16:41:12 -0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -5,9 +5,19 @@ require_once "../inc/cabecalho-admin.php";
 
 <article class="p-5 my-4 rounded-3 bg-white shadow">
     <div class="container-fluid py-1">        
-        <h2 class="display-4">Olá usuário!</h2>
+        <h2 class="display-4">Olá <?=$_SESSION['nome']?>!<DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html></h2>
         <p class="fs-5">Você está no <b>painel de controle e administração</b> do
-		site Microblog e seu <b>nível de acesso</b> é <span class="badge bg-dark"> admin/editor </span>.</p>
+		site Microblog e seu <b>nível de acesso</b> é <span class="badge bg-dark"> <?=$_SESSION['tipo']?> </span>.</p>
         <hr class="my-4">
 
         <div class="d-grid gap-2 d-md-block text-center">
