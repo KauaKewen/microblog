aa9c7947f688d51dba3c400430a0001611df3981
kewen <kauakewen1010@hotmail.com>
2024-06-11 14:26:50 -0300

    Update funcoes-noticias.php

--- inc/funcoes-noticias.php
+++ inc/funcoes-noticias.php
@@ -54,7 +54,7 @@ function lerNoticias($conexao, $idUsuario, $tipoUsuario){
         ORDER BY data DESC";
     } else {
         // Editor pode ver SOMENTE o DELE/DELA
-        $sql = "SELECT titulo, data, id";
+        $sql = "SELECT titulo, data, id FROM noticias WHERE  usuario_id = $idUsuario ORDER BY data DESC";
     }
 
    
