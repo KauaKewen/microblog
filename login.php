1741b6dd31ee7b2bd655f7f2a47409b7963794fd
kewen <kauakewen1010@hotmail.com>
2024-06-13 16:28:16 -0300

    Update login.php

--- login.php
+++ login.php
@@ -20,8 +20,8 @@ if(isset($_POST['entrar'])){
 	}
 
 	// Capturando os dados
-	$email = $_POST['email'];
-	$senha = $_POST['senha'];
+	$email = mysqli_real_escape_string($conexao, $_POST['email']) ;
+	$senha = mysqli_real_escape_string($conexao, $_POST['senha']) ;
 
 	// 1) Buscando no banco de dados através do e-mail digitado, se existe um usuário cadastrado.
 	$usuario = buscarUsuario($conexao, $email);
